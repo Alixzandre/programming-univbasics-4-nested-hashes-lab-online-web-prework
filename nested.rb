f494f5bc0f3733dc11d48fa4c4822afb5ab39343
Alexander Bispo <alexbispo173@gmail.com>
2019-09-02 01:30:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,12 +15,13 @@ def hopper
           :languages => ["C"]
         }
      }
-
+  return programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	
 	
 	programmer_hash = 
  		{
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,17 +57,16 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  return programmer_hash[:dennis_ritchie][:languages][0] 
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 	programmer_hash = 
  		{
+ 		    :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+          },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -80,7 +81,8 @@ def adding_matz
         }
      }
 
-    
+  
+  return programmer_hash
 end
 
 def changing_alan
